db8a4af35c13caa12a8d0b905154d3d5f4984236
Ayanon Fukui <springflower444422@gmail.com>
2020-05-08 17:17:08 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by 福井彩乃 on 2020/05/08.
+//  Copyright © 2020 Fukui Ayanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
